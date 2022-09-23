59a6c7d639f4fd6e691d517f9c8f2b8ea02ab6ce
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-09-23 15:39:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          {/* Si jammais on a mis une mauvaise URL (*) */}
+          <Route path="/*" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+        cupiditate corporis quo! Saepe, dolorem alias. Ipsam velit cupiditate
+        nulla. Facere in officia maiores eos, cumque neque. Autem, aperiam!
+        Dolore unde deserunt sit quo enim, amet molestias cum voluptate id eos
+        quae consequuntur nisi in dolorum facere omnis tempora quas illum quasi
+        minus? Facilis nesciunt ullam molestias expedita, dolorum excepturi
+        provident facere? Cum eveniet corporis mollitia! Reiciendis temporibus
+        ea quae rerum doloremque? Perferendis impedit odit eum molestias non
+        minima, aliquid cupiditate recusandae quas nam voluptates quaerat qui
+        error excepturi aliquam velit assumenda laboriosam? Ea tenetur
+        reprehenderit exercitationem pariatur voluptas. Ex, atque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, odit
+        a ea quidem autem veniam nam voluptates repudiandae! Suscipit dolores
+        accusamus minima eaque explicabo, ab impedit ipsam perferendis
+        repellendus, debitis veniam quo mollitia! Labore enim repellat ex,
+        maiores itaque adipisci nam, veritatis officia eum dolor beatae
+        provident temporibus eius fugiat? Nihil eligendi repellendus nam odio
+        minima voluptate neque provident quos dolorem, in eum accusantium!
+        Corporis vitae praesentium id dolorum sed cumque laborum! Similique
+        voluptatibus dolorum adipisci in quae? Alias unde sed architecto hic ut
+        officia consequuntur cumque suscipit aut incidunt autem, dolore
+        obcaecati deleniti eum soluta quae non dignissimos? Odit, porro nihil
+        sint sed libero eaque rerum necessitatibus nam dolorum fuga ratione
+        impedit numquam omnis fugit cupiditate sit aliquam quidem tempora
+        ducimus adipisci dolore non. Ducimus doloribus architecto obcaecati
+        error ea, facilis dolore eligendi rerum possimus, repellat deleniti sunt
+        doloremque. Deleniti dolore, tempora in est facere corrupti sint.
+        Reprehenderit exercitationem, sint voluptatem, unde ab quasi aliquid non
+        quo voluptatibus necessitatibus cum dolore perspiciatis vero atque
+        aspernatur minima. Praesentium qui quis sunt in, harum ipsum itaque
+        necessitatibus error id minima asperiores voluptates laboriosam aliquid
+        voluptate. Repellendus, animi. Necessitatibus, quia assumenda omnis quo,
+        rem beatae cum obcaecati, vero aspernatur illum vitae consequatur?
+      </p>
+    </div>
+  );
+};
+
+export default About;
